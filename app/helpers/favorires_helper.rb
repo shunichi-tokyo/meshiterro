8a977ef5c4c96f8980cac8f59ee1733b1f3ee63d
EC2 Default User <ec2-user@ip-172-31-47-189.ap-northeast-1.compute.internal>
2023-09-09 14:51:41 +0000

    [Add]24章終了

---
+++ app/helpers/favorires_helper.rb
@@ -0,0 +1,2 @@
+module FavoriresHelper
+end
---
+++ test/controllers/favorires_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class FavoriresControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
